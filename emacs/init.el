25f4fcb74f81b3dea23755fe0fb8422b72220a6f
Sasha Hakakha <sashahakakha@gmail.com>
2025-01-03 22:43:35 -0800

    default font mononoki

--- emacs/init.el
+++ emacs/init.el
@@ -23,7 +23,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:background "black"))))
+ '(default ((t (:background "black" :family "mononoki" :height 100))))
  '(rainbow-delimiters-depth-1-face ((t (:foreground "magenta" :inherit rainbow-delimiters-base-face)))))
 
 ;; Appearence
