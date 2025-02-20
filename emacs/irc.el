244c4631ddc6d9baff52008730b43dad43e19aaa
Sasha Hakakha <sashahakakha@gmail.com>
2025-02-19 20:30:42 -0800

    auto join irc

---
+++ emacs/irc.el
@@ -0,0 +1,31 @@
+;;; irc.el --- manages irc stuff with erc (for now)  -*- lexical-binding: t; -*-
+
+;;; Commentary:
+
+;; a binding to log into libera and join favorite rooms, etc
+
+;; Author:  Sasha Hakakha
+
+;;; Code:
+(setq erc-modules '(services autojoin button completion fill irccontrols list match menu
+                             move-to-prompt netsplit networks readonly ring stamp track)
+      erc-nick "k0zy"
+      erc-use-auth-source-for-nickserv-password t
+      erc-autojoin-channels-alist '(("libera.chat"
+                                     "#emacs" "#emacs-social" "#linux" "#archlinux" "#haskell" "#invidious" "networking" "security"))
+
+      erc-hide-list '("JOIN" "PART" "QUIT")
+      erc-current-nick-highlight-type 'nick
+      erc-keywords nil
+      erc-track-exclude-types '("JOIN" "PART" "QUIT" "NICK" "MODE")
+      erc-track-use-faces t
+      erc-track-faces-priority-list '(erc-current-nick-face erc-keyword-face)
+      erc-track-priority-faces-only 'all)
+
+(defun start-erc ()
+  "Start erc and autojoin various channels"
+  (interactive)
+  (erc :server "irc.libera.chat" :port 6667))
+
+(provide 'irc)
+;;; irc.el ends here
